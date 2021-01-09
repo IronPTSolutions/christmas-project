cbfb6faf16878a1450b0cb5cb800cea0f086529c
Julio García Pérez <julio.garciaperez@telefonica.com>
2021-01-09 13:56:23 +0100

    feat: add mongoose

--- models/post.model.js
+++ models/post.model.js
@@ -1,66 +1,22 @@
-class Post {
-    static posts = [];
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-    static find() {
-        return Promise.resolve(this.posts);
-    }
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        default: 'https://antitrustlair.files.wordpress.com/2013/02/post_danmark.jpg?w=420&h=279',
+    },
+    text: {
+        type: String,
+        required: true,
+        minlength: 10,
+    },
+});
 
-    static create(post) {
-        if (
-            typeof post !== 'object' ||
-            typeof post.title !== 'string' ||
-            typeof post.text !== 'string' ||
-            typeof post.image !== 'string'
-        ) {
-            return Promise.reject('invalid post');
-        }
-
-        const newPost = this.posts.push({
-            title: post.title,
-            text: post.text,
-            image: post.image,
-            id: (this.posts.length + 1).toString(),
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        });
-
-        return Promise.resolve(newPost);
-    }
-
-    static findById(id) {
-        const post = this.posts.find((p) => p.id === id);
-
-        return Promise.resolve(post);
-    }
-
-    static findByIdAndDelete(id) {
-        const post = this.posts.find((p) => p.id === id);
-
-        if (post) {
-            this.posts = this.posts.filter((p) => p !== post);
-        }
-
-        return Promise.resolve(post);
-    }
-
-    static findByIdAndUpdate(id, post) {
-        if (
-            typeof post !== 'object' ||
-            typeof post.title !== 'string' ||
-            typeof post.text !== 'string' ||
-            typeof post.image !== 'string'
-        ) {
-            return Promise.reject('invalid post');
-        }
-
-        const targetPost = this.posts.find((p) => p.id === id);
-
-        if (targetPost) {
-            Object.assign(targetPost, post);
-        }
-
-        return Promise.resolve(targetPost);
-    }
-}
+const Post = mongoose.model('Post', schema);
 
 module.exports = Post;
